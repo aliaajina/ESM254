bc23d1947481fab2f7053a0319de97d1e9c1b286
Alia Ajina <aliaajina@gmail.com>
2023-04-26 10:29:18 -0700

    wokring through assignment 1

--- Assignment_1/ajina_homework1.R
+++ Assignment_1/ajina_homework1.R
@@ -52,78 +52,17 @@ sumN(N=100)
 # Sum = 5,050; which is the same as sum(100) function, as expected
 
 ## Question 5
-
-# hint: ?matrix
+# Matrix function
+#' @param tot.row total number of rows
+#' @param tot.col total number of col
+#' @return answer, a matrix by column
 
 increasing.mat <- function(tot.row, tot.col) {
+
+  N <- tot.row*tot.col # Local variable to use for the matrix
+  answer <- matrix(data = (1:N), nrow = tot.row, ncol = tot.col, byrow = FALSE)
   
-  N <- tot.row*tot.col # use this local variable for the data argument of matrix()
-  answer <- # use the matrix() function here
-    
   return(answer)
 }
 
 increasing.mat(tot.row=4, tot.col=5)
-
-#### Part 2
-
-install.packages("MQMF")
-library(MQMF)
-
-## Section 1: growth curve function
-
-growth.curve <- function(p, age.data) {
-  
-  Sim.Length.Data <- p[1] * (1 - exp(-p[2] * (age.data - p[3])))
-  
-  return(Sim.Length.Data)
-}
-
-# create an object that defines the p input to growth.curve()
-pars <- c(75,0.1,-10.0,3.5) # these will be given to the p argument later
-plabels <- c("Linf","K","t0","sigma")
-
-
-# Section 2:
-# let's load in a practice data set
-
-data(minnow)
-week <- minnow$week
-length <- minnow$length
-
-# plot the experimental data
-plot(x=week,
-     y=length,
-     ylab="Body Length (mm)",
-     xlab="Age (Weeks)",
-     pch=19,
-     main="Minnow")
-
-
-# Let's look at an example for simulated data using some stand-in parameter values
-example.sim.Length <- growth.curve(p=pars[1:3], age.data=week)
-lines(x=week, y=example.sim.Length, lwd=2, col="darkgray")
-
-
-# Section 3: 
-# Let's use a statistical model to estimate parameter values for growth.curve() to use
-
-best.growth.est <- nlm(f=negNLL,
-                       p=pars,
-                       funk=growth.curve,
-                       age.data=week,
-                       observed=length,  
-                       typsize=magnitude(pars))  
-
-
-# The estimated parameter values:
-best.growth.est$estimate[1:3]
-
-# The parameter values we used to draw a line earlier
-pars[1:3]
-
-
-# Section 4 - how well do the estimated parameter values perform?
-predicted.Length <- growth.curve(best.growth.est$estimate,0:160)  
-lines(x=0:160, y=predicted.Length, lwd=2, col="red")
-
